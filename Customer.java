1095441f7286ed625d85edd0720db6827ffffa55
thais-andrade <thais.a.r.soares.bh@gmail.com>
2022-06-22 15:53:26 -0300

    Commit 3

--- Customer.java
+++ Customer.java
@@ -47,20 +47,20 @@ public class Customer {
      return result;
    }
 
-   private double amountFor(Rental each) {
-    switch (each.getMovie().getPriceCode()) {
+   private double amountFor(Rental aRental) {
+    switch (aRental.getMovie().getPriceCode()) {
            case Movie.REGULAR:
               thisAmount += 2;
-              if (each.getDaysRented() > 2)
-                 thisAmount += (each.getDaysRented() - 2) * 1.5;
+              if (aRental.getDaysRented() > 2)
+                 thisAmount += (aRental.getDaysRented() - 2) * 1.5;
               break;
            case Movie.NEW_RELEASE:
-              thisAmount += each.getDaysRented() * 3;
+              thisAmount += aRental.getDaysRented() * 3;
               break;
            case Movie.CHILDRENS:
               thisAmount += 1.5;
-              if (each.getDaysRented() > 3)
-                 thisAmount += (each.getDaysRented() - 3) * 1.5;
+              if (aRental.getDaysRented() > 3)
+                 thisAmount += (aRental.getDaysRented() - 3) * 1.5;
                break;
         }
    }
