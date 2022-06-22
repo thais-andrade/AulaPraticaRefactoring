de302454c527c50b68ab004fba44bbc82e673572
thais-andrade <thais.a.r.soares.bh@gmail.com>
2022-06-22 16:42:19 -0300

    Commit 12

--- Movie.java
+++ Movie.java
@@ -38,22 +38,7 @@ public class Movie {
   }
 
   public double getCharge(int daysRented){
-       switch (getPriceCode()) {
-           double charge = 0.0;
-           case REGULAR:
-              charge += 2;
-              if (daysRented > 2)
-                 charge += (daysRented - 2) * 1.5;
-              return charge;
-           case NEW_RELEASE:
-              charge += daysRented * 3;
-              return charge;
-           case CHILDRENS:
-              charge += 1.5;
-              if (daysRented > 3)
-                 charge += (daysRented - 3) * 1.5;
-               return charge;
-        }
+       return _price.getCharge(daysRented);
    }
 
    public int getFrequentRenterPoints(int daysRented){
