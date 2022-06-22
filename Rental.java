7d9d021ec40b865cd717dc48dc111c5886961ae2
thais-andrade <thais.a.r.soares.bh@gmail.com>
2022-06-22 16:02:43 -0300

    Commit 6

--- Rental.java
+++ Rental.java
@@ -33,4 +33,11 @@ public class Rental {
                break;
         }
    }
+
+   public int getFrequentRenterPoints() { 
+      // add bonus for a two day new release rental
+        if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+            each.getDaysRented() > 1) return 2;
+        return 1;
+   }
 }
